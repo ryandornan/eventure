129ab2312a7934f41613bae0804a6f727f9578c8
mjhs1999 <138990797+mjhs1999@users.noreply.github.com>
2024-01-29 19:28:49 +0000

    Contact submission route added

--- src/api/models.py
+++ src/api/models.py
@@ -20,3 +20,8 @@ class Event(db.Model):
     category = db.Column(db.String(50), nullable=False)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
     promoter_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    
+class ContactSubmission(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), nullable=False)
+    message = db.Column(db.Text, nullable=False)
